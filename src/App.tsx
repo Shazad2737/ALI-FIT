d691cbcff2e07b02a5aacc8136221530718dc453
Shazad2737 <shazad2737@gmail.com>
2026-08-01 12:13:45 +0530

    initialsed shadcn

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() 
+{
+  return (
+    < main className="min-h-screen bg-slate-950 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-green-500"> FIT ALI 💪</h1>
+      
+       <p className="mt-4 text-slate-400">
+          Your Personal Fitness Dashboard
+        </p>
+      </div>
+    </main>
+  );
+}
+export default App;
